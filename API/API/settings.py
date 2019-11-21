375c73106d47b1858576470dc07bc0af5a90fd93
sherikopec <sherikopec@gmail.com>
2019-11-21 20:29:13 +0800

    added django api view for users

--- API/API/settings.py
+++ API/API/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'petter.apps.PetterConfig',
+    'rest_framework',
 
 ]
 
--- API/petter/views.py
+++ API/petter/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from petter.models import User
+from petter.serializers import UserSerializer
+from rest_framework import generics
 
 # Create your views here.
+
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
